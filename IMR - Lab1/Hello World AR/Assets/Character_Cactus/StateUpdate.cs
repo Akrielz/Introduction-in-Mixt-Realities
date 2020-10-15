928380b77f9fcfc86651ef1aad6d7dc0e96f842a
Alexandru Știrbu <34741250+Akrielz@users.noreply.github.com>
2020-10-15 19:36:40 +0300

    Added Lab1

---
+++ IMR - Lab1/Hello World AR/Assets/Character_Cactus/StateUpdate.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+//using System.Diagnostics;
+using UnityEngine;
+
+public class StateUpdate : MonoBehaviour{
+    private Animator animator;
+
+    void Start(){
+        animator = GetComponent<Animator>();
+    }
+
+    void FixedUpdate(){
+        GameObject camera = GameObject.Find("ARCamera");
+        Transform cameraTransform = camera.transform;
+        Vector3 cameraPosition = cameraTransform.position;
+
+        Vector3 cactusPosition = transform.position;
+        float distance = Vector3.Distance(cameraPosition, cactusPosition);
+
+        if (Input.GetKeyDown("space"))
+        {
+            Debug.Log(distance);
+        }
+
+        bool isTooClose = distance < 5;
+        animator.SetBool("isTooClose", isTooClose);
+    }
+}
